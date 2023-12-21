9729be00af7cb6e8c3433205706e70d4226d0ca9
Nikhil <tymnec@gmail.com>
2023-12-21 14:47:22 +0530

    testing 2

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,44 @@ function App() {
   const remoteVideoRef = useRef(null);
   const currentUserVideoRef = useRef(null);
   const peerInstance = useRef(null);
+  const [callinfo, setCallinfo] = useState(null);
 
-  useEffect(() => {
+  const Create = () => {
     const peer = new Peer();
-
     peer.on("open", (id) => {
       setPeerId(id);
     });
 
-    peer.on("call", (call) => {
-      var getUserMedia =
-        navigator.getUserMedia ||
-        navigator.webkitGetUserMedia ||
-        navigator.mozGetUserMedia;
+    peer.on("call", async (call) => {
+      // Make answer call button visible
+      if (call) {
+        document.getElementById("answer-call-button").style.display = "block";
 
-      getUserMedia({ video: true, audio: true }, (mediaStream) => {
-        currentUserVideoRef.current.srcObject = mediaStream;
-        currentUserVideoRef.current.play();
-        call.answer(mediaStream);
-        call.on("stream", function (remoteStream) {
-          remoteVideoRef.current.srcObject = remoteStream;
-          remoteVideoRef.current.play();
-        });
+        setCallinfo(call);
+      }
+    });
+  };
+
+  const answerCall = () => {
+    var call = callinfo;
+    var getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
+
+    getUserMedia({ video: true, audio: true }, (mediaStream) => {
+      currentUserVideoRef.current.srcObject = mediaStream;
+      currentUserVideoRef.current.play();
+      call.answer(currentUserVideoRef.current.srcObject);
+
+      call.on("stream", function (remoteStream) {
+        remoteVideoRef.current.srcObject = remoteStream;
+        remoteVideoRef.current.play();
       });
     });
+  };
 
-    peerInstance.current = peer;
-  }, []);
-
-  const call = (remotePeerId) => {
+  const callHandler = (remotePeerId) => {
     var getUserMedia =
       navigator.getUserMedia ||
       navigator.webkitGetUserMedia ||
@@ -46,7 +55,7 @@ function App() {
       currentUserVideoRef.current.srcObject = mediaStream;
       currentUserVideoRef.current.play();
 
-      const call = peerInstance.current.call(remotePeerId, mediaStream);
+      var call = peerInstance.current.call(remotePeerId, mediaStream);
 
       call.on("stream", (remoteStream) => {
         remoteVideoRef.current.srcObject = remoteStream;
@@ -58,6 +67,12 @@ function App() {
   return (
     <div className="container border shadow rounded-2xl">
       <div className="flex gap-3 place-items-center justify-center m-5">
+        <button
+          className="p-3 rounded-2xl shadow hover:bg-black hover:text-white"
+          onClick={() => Create()}
+        >
+          Create
+        </button>
         <h1 className="font-bold">
           Your id is <span className="font-normal">{peerId}</span>
         </h1>
@@ -79,18 +94,27 @@ function App() {
         className="border shadow rounded-2xl m-5 p-3"
       />
       <button
-        onClick={() => call(remotePeerIdValue)}
+        onClick={() => callHandler(remotePeerIdValue)}
         className="mt-2 p-3 rounded-2xl shadow hover:bg-black hover:text-white"
       >
         Call
       </button>
+
+      <button
+        id="answer"
+        className="mt-2 p-3 rounded-2xl shadow hover:bg-black hover:text-white"
+        onClick={() => answerCall()}
+        hidden
+      >
+        Answer
+      </button>
       <div className="border rounded-3xl shadow p-3 m-3">
         <p className="font-bold">Local Video</p>
-        <video ref={currentUserVideoRef} />
+        <video ref={currentUserVideoRef} className="rounded-2xl" />
       </div>
       <div className="border rounded-3xl shadow p-3 m-3">
         <p className="font-bold">Remote Video</p>
-        <video ref={remoteVideoRef} />
+        <video ref={remoteVideoRef} className="rounded-2xl" />
       </div>
 
       <div></div>
